3226a2b318188ab2c46dcf0cce61c4703ee6bf97
Kenneth de Silva <kenndesilva1@gmail.com>
2019-06-19 08:25:47 +1000

    Cleaned code and added comments.
    Created comparision between logit and dl predictors.

--- creditcard.R
+++ creditcard.R
@@ -1,31 +1,40 @@
-# library(dplyr)
-library(ggplot2)
+# Packages
+library(dplyr)
 library(keras)
 
+# Import data
 credit_card.df <- read.csv("./creditcard.csv")
 
-tr <- sample(nrow(credit_card.df), 100000)
-nw <- sample(nrow(credit_card.df), 100000)
+# Create training and test data sets
+tr <- sample(nrow(credit_card.df), 100000, replace = T)
+nw <- sample(nrow(credit_card.df), 100000, replace = T)
 
-credit_card.lm <- lm(Class ~ ., data = credit_card.df)
+tr_data <- as.matrix(credit_card.df[tr, c(-1, -31)])
+tr_output <- credit_card.df[tr, 31]
+nw_data <- as.matrix(credit_card.df[nw, c(-1, -31)])
+nw_output <- credit_card.df[nw, 31]
+
+# Create regression models
+credit_card.lm <- lm(Class ~ ., data = credit_card.df[tr, ])
 credit_card.logit <- glm(Class ~ .,
-                          data = credit_card.df,
+                          data = credit_card.df[tr, ],
                           family = binomial(link = "logit"))
 
+# Create classifier for logit
 credit_card.logit.pr <-
     as.numeric(credit_card.logit$fitted.values > 0.5)
-credit_card.conf <- addmargins(table(credit_card.df$Class,
+
+# Confusion table for logit predictor
+credit_card.logit.conf <- addmargins(table(credit_card.df[nw, 'Class'],
                                      credit_card.logit.pr))
 
-credit_card.conf
-
+# Predicting with regression models
 credit_card.lm.pr <- predict(credit_card.lm, credit_card.df[nw, ])
 credit_card.logit.pr <- predict(credit_card.logit, credit_card.df[nw, ])
 
-# Less go deep learning time :)
-
-model <- keras_model_sequential() %>%
-	layer_dense(units = 10, activation = "relu", input_shape = c(29)) %>%
+# Create deep learning model
+credit_card.dl <- keras_model_sequential() %>%
+	layer_dense(units = 20, activation = "relu", input_shape = c(29)) %>%
 	layer_dense(units = 10, activation = "relu") %>%
 	layer_dense(units = 1, activation = "sigmoid") %>%
 	compile(
@@ -34,17 +43,18 @@ model <- keras_model_sequential() %>%
 		metrics = c('binary_accuracy')
 	)
 
-tr_data <- as.matrix(credit_card.df[tr, c(-1, -31)])
-tr_output <- credit_card.df[tr, 31]
-nw_data <- as.matrix(credit_card.df[nw, c(-1, -31)])
-nw_output <- credit_card.df[nw, 31]
-
-history <- model %>%
+# Train dl model
+history <- credit_card.dl %>%
 	fit(tr_data,
 		tr_output,
-		epochs = 10,
+		epochs = 30,
 		batch_size = 100)
 
-model %>% evaluate(nw_data, nw_output)
-model.predict <- model %>% predict_classes(nw_data)
-addmargins(table(model.predict, nw_output))
+# Evaluate dl model
+credit_card.dl %>% evaluate(nw_data, nw_output)
+credit_card.dl.predict <- credit_card.dl %>% predict_classes(nw_data)
+credit_card.dl.conf <- addmargins(table(credit_card.dl.predict, nw_output))
+
+# Comparision of Logit and DL predictors
+credit_card.logit.conf
+credit_card.dl.conf
