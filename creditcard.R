42779b7c2075f30d5c39b52bec42d858765b06ae
Kenneth de Silva <kenndesilva1@gmail.com>
2019-06-21 12:05:45 +1000

    Hdf5 file and dl modifications.
    Made a more direct comparison between logit and dl predictors. Also
    added a weights + biases file and made 10 (lol) hidden layers
    
    Simplified dl model.

--- creditcard.R
+++ creditcard.R
@@ -1,60 +1,67 @@
 # Packages
+library(data.table)
 library(dplyr)
 library(keras)
 
 # Import data
-credit_card.df <- read.csv("./creditcard.csv")
+df <- fread("./creditcard.csv", data.table = F)
 
 # Create training and test data sets
-tr <- sample(nrow(credit_card.df), 100000, replace = T)
-nw <- sample(nrow(credit_card.df), 100000, replace = T)
+tr <- sample(nrow(df), 10^5, replace = F)
+nw <- sample(nrow(df), 10^5, replace = F)
 
-tr_data <- as.matrix(credit_card.df[tr, c(-1, -31)])
-tr_output <- credit_card.df[tr, 31]
-nw_data <- as.matrix(credit_card.df[nw, c(-1, -31)])
-nw_output <- credit_card.df[nw, 31]
+tr_data <- as.matrix(df[tr, c(-1, -31)])
+tr_output <- df[tr, 31]
+nw_data <- as.matrix(df[nw, c(-1, -31)])
+nw_output <- df[nw, 31]
 
 # Create regression models
-credit_card.lm <- lm(Class ~ ., data = credit_card.df[tr, ])
-credit_card.logit <- glm(Class ~ .,
-                          data = credit_card.df[tr, ],
+lm <- lm(Class ~ ., data = df[tr, ])
+logit <- glm(Class ~ .,
+                          data = df[tr, ],
                           family = binomial(link = "logit"))
 
 # Create classifier for logit
-credit_card.logit.pr <-
-    as.numeric(credit_card.logit$fitted.values > 0.5)
+logit.pr <-
+    as.numeric(logit$fitted.values > 0.5)
 
-# Confusion table for logit predictor
-credit_card.logit.conf <- addmargins(table(credit_card.df[nw, 'Class'],
-                                     credit_card.logit.pr))
-
-# Predicting with regression models
-credit_card.lm.pr <- predict(credit_card.lm, credit_card.df[nw, ])
-credit_card.logit.pr <- predict(credit_card.logit, credit_card.df[nw, ])
+# Load deap learning model from hdf5 file
+# dl <- load_model_hdf5("./10L-DL.hdf5")
 
 # Create deep learning model
-credit_card.dl <- keras_model_sequential() %>%
-	layer_dense(units = 20, activation = "relu", input_shape = c(29)) %>%
-	layer_dense(units = 10, activation = "relu") %>%
-	layer_dense(units = 1, activation = "sigmoid") %>%
-	compile(
-		optimizer = "sgd",
+dl <- keras_model_sequential() %>%
+	layer_dense(units = 20, activation = "elu", input_shape = c(29)) %>%
+	layer_dense(units = 10, activation = "elu") %>%
+	layer_dense(units = 5, activation = "elu") %>%
+	layer_dense(units = 1, activation = "sigmoid")
+dl %>% compile(
+		optimizer = optimizer_sgd(lr = 0.01,
+								  momentum = 0,
+								  decay = 0),
 		loss = 'binary_crossentropy',
 		metrics = c('binary_accuracy')
 	)
 
 # Train dl model
-history <- credit_card.dl %>%
+history <- dl %>%
 	fit(tr_data,
 		tr_output,
-		epochs = 30,
+		epochs = 100,
 		batch_size = 100)
 
 # Evaluate dl model
-credit_card.dl %>% evaluate(nw_data, nw_output)
-credit_card.dl.predict <- credit_card.dl %>% predict_classes(nw_data)
-credit_card.dl.conf <- addmargins(table(credit_card.dl.predict, nw_output))
+dl %>% evaluate(nw_data, nw_output)
+
+# Confusion table for logit predictor
+logit.conf <- addmargins(table(logit.pr, nw_output))
+
+# Confusion table for dl predictor
+dl.predict <- dl %>% predict_classes(nw_data)
+dl.conf <- addmargins(table(dl.predict, nw_output))
 
 # Comparision of Logit and DL predictors
-credit_card.logit.conf
-credit_card.dl.conf
+logit.conf
+dl.conf
+
+# Save model parameters to hdf5 file
+dl %>% save_model_hdf5("./10L-DL.hdf5")
